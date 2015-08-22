21dbe9cd9a452b154f8e8aab3f313091ac9fa0f0
Sal Uryasev <suryasev@salesforce.com>
2015-08-22 14:29:58 -0700

    Tokens work and stuff

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -4,7 +4,70 @@ using System.Collections.Generic;
 
 public class Player : MonoBehaviour {
 
-	public List<Token> tokenList = new List<Token>();
+	public GameObject token;
+
+	public int maxTokensLimit;
+
+	public class TokenManager {
+		private Player owner;
+		private GameObject token;
+		public TokenManager(Player player, GameObject tokenInstance) {
+			owner = player;
+			token = tokenInstance;
+		}
+
+		public List<Token> tokenList = new List<Token>();
+		
+		public void createNewToken(Vector3 mousePosition) {
+			var objectPos = Camera.main.ScreenToWorldPoint(mousePosition);
+			//objectPos.z = -5;
+			GameObject go = Instantiate(token, objectPos, Quaternion.identity) as GameObject;
+			
+			Token tk = go.GetComponent<Token>();
+			tk.Click ();
+			tk.init(owner);
+			tokenList.Insert(0,tk);
+
+		}
+
+		public Token getActiveToken() {
+			if (tokenList.Count > 0) {
+				Token activeToken = tokenList[0];
+				if (activeToken.Active()) return activeToken;
+			}
+			return null;
+		}
+
+		public bool ifBlocking() {
+			return (getActiveToken() != null);
+		}
+
+		public bool Click() {
+			Token activeToken = getActiveToken ();
+			if (activeToken != null) {
+				activeToken.Click ();
+				return true;
+			}
+			return false;
+		}
+
+		public void trimOldTokens(int maxTokens) {
+			if (tokenList.Count > maxTokens) {
+				foreach (Token tk in tokenList.GetRange (maxTokens-1, (tokenList.Count-maxTokens))) {
+					//TODO: Play destruction animation
+					tk.Destroy();
+				}
+				tokenList = tokenList.GetRange (0, maxTokens);
+			}
+		}
+	}
+
+	public Player() {
+//		Debug.Log (token);
+//		tokenManager = new TokenManager (this, token);
+	}
+
+	public TokenManager tokenManager;
 
 	public float sorrow = 100000;
 	// Use this for initialization
@@ -19,7 +82,8 @@ public class Player : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+		if (tokenManager == null) tokenManager = new TokenManager (this, token);
+		tokenManager.trimOldTokens (maxTokensLimit);
 	}
 
 
