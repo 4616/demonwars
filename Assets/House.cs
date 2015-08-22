be9879d3a7c5ef5d6c8a0d7c8535da9639679f98
4616 <dougal.michael@gmail.com>
2015-08-21 23:30:48 -0700

    as

--- Assets/House.cs
+++ Assets/House.cs
@@ -4,7 +4,7 @@ using System.Collections;
 public class House : Obstacle {
 	public float sorrow = 1;
 	public float sorrowgen = 0;
-	public float sorrowgenratechange = .01;
+	public float sorrowgenratechange = .00001;
 	public bool ownable = true;
 
 
