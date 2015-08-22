06126159583d580d1b849b3567387906c70da52e
Sean Francis-Lyon <seanflyon@gmail.com>
2015-08-21 23:27:03 -0700

    wandering

--- Assets/Minion.cs
+++ Assets/Minion.cs
@@ -2,14 +2,16 @@
 using System.Collections;
 
 public class Minion : MonoBehaviour {
+	public MinionState state;
 
 	// Use this for initialization
 	void Start () {
-	
+		state = new WanderState ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		Vector2 delta = state.setVelocity ();
+		gameObject.transform.position += new Vector3(delta.x, delta.y);
 	}
 }
---
+++ Assets/MinionState.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public interface MinionState {
+
+	Vector2 setVelocity();
+}
