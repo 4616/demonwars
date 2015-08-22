36ef36ba135e81b5d58ce349367e4311d5a72113
4616 <dougal.michael@gmail.com>
2015-08-22 11:00:53 -0700

    starting game

---
+++ Assets/GameSetup.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameSetup : MonoBehaviour {
+	public int nplayers = 2;
+	public GameObject playerprefab;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void Initialize(){
+	 }
+}
