fd23434807c040afecfd16c5a97831452015e536
Sal Uryasev <suryasev@salesforce.com>
2015-08-22 10:23:55 -0700

    Foreground controller

--- Assets/ClickForegroundController.cs
+++ Assets/ClickForegroundController.cs
@@ -4,16 +4,21 @@ using System.Collections;
 public class ClickForegroundController : MonoBehaviour {
 
 	public int delayTimer;
-	private int currentDelay;
+	private int currentDelay = 0;
 
 	public Transform token;
 
+	private Camera camera = GetComponent.<Camera>();
+
 	void OnMouseDown()
 	{
+		Debug.Log ("Potato");
 		if (currentDelay <= 0) {
 			var mousePos = Input.mousePosition;
-			var objectPos = Camera.current.ScreenToWorldPoint(mousePos);
-			Instantiate(token, objectPos, Quaternion.identity);
+			Debug.Log (mousePos.x);
+			Debug.Log (mousePos.y);
+//			var objectPos = Camera.current.ScreenToWorldPoint(mousePos);
+//			Instantiate(token, objectPos, Quaternion.identity);
 		}
 	}
 
