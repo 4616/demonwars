c408e8a4ee701834869bc305d6da6c3187114702
4616 <dougal.michael@gmail.com>
2015-08-22 02:11:23 -0700

    Main Menu

---
+++ Assets/MainMenu/Scripts/MainMenu.cs
@@ -0,0 +1,36 @@
+﻿/// <summary>
+/// Main menu.
+/// Attached to Main Camera (or so says YouTube)
+/// https://www.youtube.com/watch?v=xoaYzfCix3c
+/// </summary>
+
+using UnityEngine;
+using System.Collections;
+
+public class MainMenu : MonoBehaviour {
+	public Texture backgroudTexture;
+
+	public float guiPlacementX1 = .25f;
+	public float guiPlacementX2 = .25f;
+	public float guiPlacementY1 = .5f;
+	public float guiPlacementY2 = .7f;
+	void OnGUI(){
+		// Displays background texture
+
+		GUI.DrawTexture (new Rect (0, 0, Screen.width, Screen.height), backgroudTexture);
+
+
+		// Displays our Buttons
+					//button location x, button location y, buttion size x, button size y
+		if(GUI.Button (new Rect (Screen.width * guiPlacementX1, Screen.height * guiPlacementY1, Screen.width * .5f, Screen.height * .1f), "Play Game")){
+			print ("Clicked Play Game");
+		}
+
+		if(GUI.Button (new Rect (Screen.width * guiPlacementX2, Screen.height * guiPlacementY2, Screen.width * .5f, Screen.height * .1f), "Options")){
+			print ("Clicked Options");
+		}
+
+	}
+
+
+}
